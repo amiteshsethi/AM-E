f9ded19b49bae1bd4e53e0521072f90437bdc5e5
Amitesh Sethi <amiteshsethi@gmail.com>
2025-04-06 21:09:31 +0530

    wip: developing frontend chat screen UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import Header from './components/Header';
 import { Routes , Route} from 'react-router-dom';
 import Home from './pages/Home';
-import { Chat } from './pages/Chat';
+import Chat  from './pages/Chat';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Notfound from './pages/Notfound';
